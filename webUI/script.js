ed079d5fe1f5bcd02ca981d9b6ef0e094ec27b5f
oren villaret <oren@itbits.blog>
2024-02-04 18:39:33 +0100

    #9, #10 | we solved issue in calculation and start modal

--- webUI/script.js
+++ webUI/script.js
@@ -1,13 +1,47 @@
 let new_exercise_test_payload = {
 
     "exercise":{
-        "first": 1,
-        "second": 6
+        "numbers":[2, 4, 2],
+        "operations":["X", ":"]
     }
 }
 
 function new_exercise(){
-    let exercise_string = new_exercise_test_payload.exercise.first + " X " + new_exercise_test_payload.exercise.second
+    // let exercise_string = new_exercise_test_payload.exercise.first.toString() + new_exercise_test_payload.exercise.operation + new_exercise_test_payload.exercise.second.toString()
+    let exercise_string = ""
+    let i = 0
+    let result = new_exercise_test_payload.exercise.numbers[0]
+
+    while (i < new_exercise_test_payload.exercise.numbers.length){
+        exercise_string += new_exercise_test_payload.exercise.numbers[i].toString()
+        if (i < new_exercise_test_payload.exercise.operations.length) {
+            exercise_string +=  " " + new_exercise_test_payload.exercise.operations[i] + " "
+        }
+
+        if (i > 0){
+            switch(new_exercise_test_payload.exercise.operations[i-1]){
+                case "X":
+                    result *= new_exercise_test_payload.exercise.numbers[i]
+                    break
+                case ":":
+                    result /= new_exercise_test_payload.exercise.numbers[i]
+                    break
+                // case "+":
+                //     result += new_exercise_test_payload.exercise.numbers[i]
+                //     console.log(result, "+")
+                //     break
+                // case "-":
+                //     result -= new_exercise_test_payload.exercise.numbers[i]
+                //     console.log(result, "-")
+                //     break
+            }
+        }
+        
+        i++
+    }        
 
     document.getElementById("ExerciseText").innerHTML = exercise_string
+
+    console.log(result)
+
 }
